74bd85b321f79a2192e3cfd864f6257c2dd8315e
rqdasilva <rqdasilva@gmail.com>
2018-05-31 16:11:40 -0400

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,12 +1,4 @@
 node default {
-  file {'/root/README':
-    ensure  => file,
-    content => 'This is a readme',
-    owwer   => 'root',
-  }
-  file {'/root/README':
-   owner => 'root',
-   }
 }
  
  
